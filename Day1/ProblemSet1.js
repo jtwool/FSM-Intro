6035a48c509a06ff7d2d2a1ab402eeb8294c83fc
JT W <jwolohan@gmail.com>
2018-05-19 20:51:29 -0400

    ProblemSet1.js added

---
+++ Day1/ProblemSet1.js
@@ -0,0 +1,60 @@
+// Problem Set One: Integers and Functions
+// FSM Intro to Programming, Summer '18
+// (c) J.T. Wolohan, 2018
+// MPL 2.0
+
+// Problem 1
+double = function(x){
+  // Return x doubled
+}  
+  
+// Problem 2
+half = function(x){
+ // Return half of x 
+}
+
+// Problem 3
+square = function(x){
+  // Return x times x
+}
+
+// Problem 4
+circ_from_radius = function(r){
+ // The circumference of a circle can be found using its radius
+ // the formula for that is circumference = 2 * pi * r
+ // Return the circumference of a cricle given r
+ pi = 3.14
+}
+
+// Problem 5
+diameter_from_area = function(area){
+  // A circle's diameter can be found using its area
+  // The diameter of a circle is twice its radius
+  // So the following formula retrieves the diameter
+  // diameter = sqrt(area/pi)/2
+  // the square root function is Math.sqrt(), e.g., Math.sqrt(4) -> 2
+  pi = 3.14
+}
+
+// Problem 6
+pythagorean_c = function(a,b){
+ // The Pythagorean theorem states that a*a = b*b + c*c
+ // Return c, given a and b
+}
+
+// Problem 7
+pythagorean_b = function(c,a){
+ // The Pythagorean theorem states that a*a = b*b + c*c
+ // Return b, given a and c
+}
+
+
+// Problem 8
+distance = function(x1,y1,x2,y2){
+ // The Pythagorean theroem can be used to find the distance between two points
+ // Point 1 at (x1,y1) and point 2 at (x2, y2)
+ // We do this by treating the difference between the x-values as a and the 
+ // difference between the y-values as b.
+ // Return the distance, given two points
+
+}
