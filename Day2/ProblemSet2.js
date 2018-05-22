6d759050557a871cd4c813ee955072d15987137d
JT W <jwolohan@gmail.com>
2018-05-22 11:54:23 -0400

    Added day 2 lecture and problem set.

---
+++ Day2/ProblemSet2.js
@@ -0,0 +1,60 @@
+// Problem Set One: Integers and Functions
+// FSM Intro to Programming, Summer '18
+// (c) J.T. Wolohan, 2018
+// MPL 2.0
+
+// Problem 1
+firstLetter = function(s){
+  // Return first letter of a string
+}  
+  
+// Problem 2
+toWords = function(s){
+ // Use the split method to turn a string into words
+ // Return those words
+}
+
+// Problem 3
+secondWord = function(s){
+  // Use the split method to turn a string into words
+  // Then return only the second word
+}
+
+// Problem 4
+addStrings = function(s1,s2){
+ // We can add two strings together with +
+ // "Hello " + "world" => "Hello world"
+ // return s1 and s2 added together with a space in between
+}
+
+// Problem 5
+initials = function(firstName,lastName){
+  // A person's initials are the first letter of their first name
+  // and the first letter of their last name
+  // take a string for each name and
+  // return their intials with periods in between
+  // "JT" "Wolohan" => "J.W."
+}
+
+// Problem 6
+initials2 = function(full_name){
+ // Use the split method, e.g., full_name.split(" ")
+ // To create intials from someone's full name as one string
+ // "John Thomas Wolohan" => "J.W."
+}
+
+// Problem 7
+flip_name = function(full_name){
+ // In some parts of the world, the family name comes first not last
+ // Take the full name as a string and swap the first and last names
+ // "John Thomas Wolohan" => "Wolohan Thomas John"
+}
+
+// Problem 8
+snake_speak = function(s){
+ // Sssnaksss talk like thisss
+ // We can replace leteresss from ssstringsss
+ // usssing the replace method, 
+ // e.g., "string".replace(/str/g,"k") => "king"
+ // Return a string in snake speak where each "s" is replaced by "sss"
+}
